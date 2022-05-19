d1fd76e6a39f0d2be9776477f9bdd22915e800a2
guoliang <tswanggl@gmail.com>
2022-05-19 17:09:32 +0800

    update timer 1 prjs

--- Timer_Test_Prjs/Timer1_mode0/src/main.c
+++ Timer_Test_Prjs/Timer1_mode0/src/main.c
@@ -1,37 +1,25 @@
 #include <REG52.H>
 
-sbit LED1 = P0^0;
 
-
-void timer1_init()
+void timer_init()
 {
-    TMOD = TMOD & 0x0f;
-    TL1 = 0xC0;
-    TH1 = 0xE0;
-    TR1 = 1;
-    TF1 = 0;
+    TMOD &= 0x0f;
+    TMOD |= 0x10;
+
+    TH1
+    TL1
+
+    TF2
+    TR1
 }
 
-unsigned int cnt;
 
 int main()
 {
-    timer1_init();
-    ET1 = 1;
-    EA = 1;
-
-    while(1);
+    timer_init();
 }
 
-void timer1_callback() interrupt 3 
+void timer_response interrupt 3
 {
-    TL1 = 0xC0;
-    TH1 = 0xE0;
 
-    cnt++;
-    if(cnt == 1000)
-    {
-        LED1 = !LED1;
-        cnt = 0;
-    }
 }
